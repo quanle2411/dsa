17af2e730bc506a9cd048d8fdf11a10f850e3332
quanle2411 <quanle24112004@gmail.com>
2024-10-09 15:00:11 +0700

    Update Program.cs

--- ctdl.2/Program.cs
+++ ctdl.2/Program.cs
@@ -1,69 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
 
-class BrowserHistory
-{
-    private Stack<string> backStack;
-    private Stack<string> forwardStack;
-    private string current;
+class BrowserHistory {
+private:
+    stack<string> backStack, forwardStack;
+    string currentPage;
 
-    public BrowserHistory(string homepage)
-    {
-        backStack = new Stack<string>();
-        forwardStack = new Stack<string>();
-        current = homepage;
+public:
+    BrowserHistory(string homepage) {
+        currentPage = homepage;
     }
 
-    // Visit a new URL
-    public void Visit(string url)
-    {
-        backStack.Push(current);
-        current = url;
-        forwardStack.Clear(); // Clear forward history after visiting a new page
+    void visit(string url) {
+        backStack.push(currentPage);
+        currentPage = url;
+        while (!forwardStack.empty()) forwardStack.pop(); // Clear forward history
     }
 
-    // Go back by a certain number of steps
-    public string Back(int steps)
-    {
-        while (steps > 0 && backStack.Count > 0)
-        {
-            forwardStack.Push(current);
-            current = backStack.Pop();
-            steps--;
+    string back(int steps) {
+        while (steps-- > 0 && !backStack.empty()) {
+            forwardStack.push(currentPage);
+            currentPage = backStack.top();
+            backStack.pop();
         }
-        return current;
+        return currentPage;
     }
 
-    // Go forward by a certain number of steps
-    public string Forward(int steps)
-    {
-        while (steps > 0 && forwardStack.Count > 0)
-        {
-            backStack.Push(current);
-            current = forwardStack.Pop();
-            steps--;
+    string forward(int steps) {
+        while (steps-- > 0 && !forwardStack.empty()) {
+            backStack.push(currentPage);
+            currentPage = forwardStack.top();
+            forwardStack.pop();
         }
-        return current;
+        return currentPage;
     }
-}
+};
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
-
-        browserHistory.Visit("google.com");
-        browserHistory.Visit("facebook.com");
-        browserHistory.Visit("youtube.com");
-
-        Console.WriteLine(browserHistory.Back(1)); // facebook.com
-        Console.WriteLine(browserHistory.Back(1)); // google.com
-        Console.WriteLine(browserHistory.Forward(1)); // facebook.com
-
-        browserHistory.Visit("linkedin.com");
-        Console.WriteLine(browserHistory.Forward(2)); // linkedin.com (no forward history)
-        Console.WriteLine(browserHistory.Back(2)); // google.com
-        Console.WriteLine(browserHistory.Back(7)); // leetcode.com (max back)
-    }
+int main() {
+    BrowserHistory bh("uit.edu.vn");
+    bh.visit("google.com");
+    bh.visit("facebook.com");
+    bh.visit("youtube.com");
+    cout << bh.back(1) << endl;
+    cout << bh.back(1) << endl;
+    cout << bh.forward(1) << endl;
+    bh.visit("linkedin.com");
+    cout << bh.forward(2) << endl;
+    cout << bh.back(2) << endl;
+    cout << bh.back(7) << endl;
+    return 0;
 }
