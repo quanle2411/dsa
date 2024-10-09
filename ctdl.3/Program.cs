98296ed33a0f371ecae103e54a1c05f943c1fa91
quanle2411 <quanle24112004@gmail.com>
2024-10-09 15:01:18 +0700

    Update Program.cs

--- ctdl.3/Program.cs
+++ ctdl.3/Program.cs
@@ -1,70 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
+#include <iostream>
+#include <unordered_map>
+using namespace std;
 
-class AuthenticationManager
-{
-    private int timeToLive;
-    private Dictionary<string, int> tokens;
+class AuthenticationManager {
+private:
+    unordered_map<string, int> tokenExpiryMap; // tokenId -> expiryTime
+    int timeToLive;
 
-    public AuthenticationManager(int timeToLive)
-    {
-        this.timeToLive = timeToLive;
-        this.tokens = new Dictionary<string, int>();
-    }
+public:
+    AuthenticationManager(int ttl) : timeToLive(ttl) {}
 
     // Generate a new token
-    public void Generate(string tokenId, int currentTime)
-    {
-        tokens[tokenId] = currentTime + timeToLive;
+    void generate(string tokenId, int currentTime) {
+        tokenExpiryMap[tokenId] = currentTime + timeToLive;
     }
 
-    // Renew a token if it hasn't expired
-    public void Renew(string tokenId, int currentTime)
-    {
-        if (tokens.ContainsKey(tokenId) && tokens[tokenId] > currentTime)
-        {
-            tokens[tokenId] = currentTime + timeToLive;
+    // Renew a token if it has not expired
+    void renew(string tokenId, int currentTime) {
+        if (tokenExpiryMap.find(tokenId) != tokenExpiryMap.end() && tokenExpiryMap[tokenId] > currentTime) {
+            tokenExpiryMap[tokenId] = currentTime + timeToLive;
         }
     }
 
-    // Count unexpired tokens at a given time
-    public int CountUnexpiredTokens(int currentTime)
-    {
+    // Count how many tokens are still unexpired
+    int countUnexpiredTokens(int currentTime) {
         int count = 0;
-        List<string> expiredTokens = new List<string>();
-
-        foreach (var token in tokens)
-        {
-            if (token.Value > currentTime)
-            {
+        for (auto it = tokenExpiryMap.begin(); it != tokenExpiryMap.end(); ) {
+            if (it->second <= currentTime) {
+                it = tokenExpiryMap.erase(it);
+            } else {
                 count++;
-            }
-            else
-            {
-                expiredTokens.Add(token.Key); // Collect expired tokens for removal
+                it++;
             }
         }
-
-        // Remove expired tokens
-        foreach (var tokenId in expiredTokens)
-        {
-            tokens.Remove(tokenId);
-        }
-
         return count;
     }
+};
+
+int main() {
+    AuthenticationManager authManager(5);
+    authManager.renew("aaa", 1);  // no token exists, so nothing happens
+    authManager.generate("aaa", 2);  // generates token "aaa" at time 2
+    cout << authManager.countUnexpiredTokens(6) << endl;  // should output 1
+    authManager.generate("bbb", 7);  // generates token "bbb" at time 7
+    authManager.renew("aaa", 8);  // "aaa" has expired, so nothing happens
+    authManager.renew("bbb", 10);  // "bbb" is renewed to expire at time 15
+    cout << authManager.countUnexpiredTokens(15) << endl;  // should output 0
+    return 0;
 }
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        AuthenticationManager authManager = new AuthenticationManager(5);
-
-        authManager.Renew("aaa", 1);
-        authManager.Generate("aaa", 2);
-        Console.WriteLine(authManager.CountUnexpiredTokens(6)); // 1
-
-        authManager.Generate("bbb", 7);
-        authManager.Renew("aaa", 8
-S
